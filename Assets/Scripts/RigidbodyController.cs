f48c4f54dc913bbe0810918ca42b209aeb25533d
Anthony Montanez <amontanez09@gmail.com>
2019-11-09 23:43:33 -0600

    added the terrain, added materials,
    added new 3d models , added a couple assets from the store skyboxs, materials. created the level but still working on the controls

--- Assets/Scripts/RigidbodyController.cs
+++ Assets/Scripts/RigidbodyController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+//using UnityEngine.CoreModule;
 public class RigidbodyController : MonoBehaviour
 {
     Rigidbody rigidbody;
@@ -12,12 +13,19 @@ public class RigidbodyController : MonoBehaviour
     public Slider slider;
     private float Timer = 3f;
     public bool canJump;
+
+    public float timerotation;
+
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
-        airpressure = 100f;
+        airpressure = 1f;
         canJump = true;
+        anim = GetComponent<Animator>();
+        //Cursor.visble = false;
+        //Cursor.lockstate = true;
     }
 
     // Update is called once per frame
@@ -45,24 +53,60 @@ public class RigidbodyController : MonoBehaviour
             Timer = 5f;
             canJump = true;
         }
-        
-        
+        //movemnt code moved from fixed update
+        float moveHor = Input.GetAxis("Horizontal");
+        float moveVer = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHor, 0.0f, moveVer) * speed * Time.deltaTime;
+        timerotation = 1000f * Time.deltaTime;
+        Vector3 targetdir = movement;
+        targetdir.y = 0;
+        if (targetdir == Vector3.zero)
+        {
+            targetdir = transform.forward;
+            Quaternion tr = Quaternion.LookRotation(targetdir);
+            Quaternion targetRotation = Quaternion.Slerp(transform.rotation, tr, 0f);
+            transform.rotation = targetRotation;
+        }
+
+        //rigidbody.velocity = movement *speed;
+        //rigidbody.AddForce(movement * thrust
+        transform.Translate(movement, Space.Self);
 
     }
 
     void FixedUpdate()
     {
+        
         float moveHor = Input.GetAxis("Horizontal");
         float moveVer = Input.GetAxis("Vertical");
+        /*
+        Vector3 movement = new Vector3(moveHor, 0.0f, moveVer) * speed * Time.deltaTime;
+        timerotation = 1000f * Time.deltaTime ;
+        Vector3 targetdir = movement;
+        targetdir.y = 0;
+        if(targetdir == Vector3.zero)
+        {
+            targetdir = transform.forward;
+            Quaternion tr = Quaternion.LookRotation(targetdir);
+            Quaternion targetRotation = Quaternion.Slerp(transform.rotation, tr,.9f);
+            transform.rotation = targetRotation;
+        }
 
-        Vector3 movement = new Vector3(moveHor, 0.0f, moveVer) * speed;
+        //rigidbody.velocity = movement *speed;
+        //rigidbody.AddForce(movement * thrust
+        transform.Translate(movement, Space.Self);
 
-        //rigidbody.velocity = movement;
-        rigidbody.AddForce(movement * thrust);
+    */
+        if(moveVer >= 0.1f)
+        {
+            anim.Play("Walk");
+        }
 
         if (Input.GetKey(KeyCode.Space) && canJump == true)
         {
             jump();
+            anim.Play("Jump");
         }
     }
     void jump() {
